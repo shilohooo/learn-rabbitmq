b08e0de59431e1a03af446db6a33f7f5f57f1e37
shiloh <lixiaolei595@gmail.com>
2022-04-20 22:11:58 +0800

    feat: add rabbitmq const

---
+++ hello-world/src/main/java/org/shiloh/rabbitmq/constant/RabbitMqConstants.java
@@ -0,0 +1,48 @@
+package org.shiloh.rabbitmq.constant;
+
+/**
+ * RabbitMQ 常量
+ *
+ * @author shiloh
+ * @date 2022/4/20 21:35
+ */
+public final class RabbitMqConstants {
+    private RabbitMqConstants() {
+
+    }
+
+    /**
+     * 队列名称
+     * <p>
+     * 生产者将消息发送到此队列中，消费者将接收该队列中的消息
+     */
+    public static final String QUEUE_NAME = "Hello";
+
+    /**
+     * MQ 服务器相关常量
+     *
+     * @author shiloh
+     * @date 2022/4/20 21:36
+     */
+    public static class Server {
+        /**
+         * RabbitMQ 服务器 IP
+         */
+        public static final String IP = "1.12.253.192";
+
+        /**
+         * RabbitMQ 服务器端口
+         */
+        public static final int PORT = 5672;
+
+        /**
+         * 用户名
+         */
+        public static final String USERNAME = "shiloh";
+
+        /**
+         * 密码
+         */
+        public static final String PASSWORD = "Lixiaolei1998#@";
+    }
+}
