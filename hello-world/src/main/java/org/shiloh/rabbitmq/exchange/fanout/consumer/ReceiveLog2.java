d9df58cb9c9ed9a1d135843e4b946bf449eab5fd
shiloh <lixiaolei595@gmail.com>
2022-05-06 21:09:46 +0800

    refactor(交换机): 常量重命名、添加注释

--- hello-world/src/main/java/org/shiloh/rabbitmq/exchange/fanout/consumer/ReceiveLog2.java
+++ hello-world/src/main/java/org/shiloh/rabbitmq/exchange/fanout/consumer/ReceiveLog2.java
@@ -1,4 +1,4 @@
-package org.shiloh.rabbitmq.exchange.consumer;
+package org.shiloh.rabbitmq.exchange.fanout.consumer;
 
 import com.rabbitmq.client.CancelCallback;
 import com.rabbitmq.client.Channel;
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeoutException;
 
 import static com.rabbitmq.client.BuiltinExchangeType.FANOUT;
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.shiloh.rabbitmq.constant.RabbitMqConstants.LOG_MSG_EXCHANGE_NAME;
+import static org.shiloh.rabbitmq.constant.RabbitMqConstants.LOG_MSG_FANOUT_EXCHANGE_NAME;
 
 /**
  * 交换机测试 - 日志消费者02
@@ -25,13 +25,13 @@ public class ReceiveLog2 {
             // 获取 channel
             final Channel channel = RabbitMqUtils.getChannel();
             // 声明一个交换机，指定交换机的名称，以及指定交换机的类型为发布/订阅（fanout）类型。
-            channel.exchangeDeclare(LOG_MSG_EXCHANGE_NAME, FANOUT.getType());
+            channel.exchangeDeclare(LOG_MSG_FANOUT_EXCHANGE_NAME, FANOUT.getType());
             // 创建一个临时队列，该队列的名称是随机的
             // 当消费者断开与队列的连接后，该队列会被自动删除
             final String queue = channel.queueDeclare()
                     .getQueue();
             // 绑定交换机与队列的关系
-            channel.queueBind(queue, LOG_MSG_EXCHANGE_NAME, "");
+            channel.queueBind(queue, LOG_MSG_FANOUT_EXCHANGE_NAME, "");
             System.out.println("日志消费者02等待接收消息，把接收到的消息打印到控制台~");
 
             // 接收消息成功回调处理
