e0ee518f50618621dac5bb01643ca4f546a4bd7f
shiloh <lixiaolei595@gmail.com>
2022-05-06 21:09:31 +0800

    feat(交换机): 新增直接交换机案例

---
+++ hello-world/src/main/java/org/shiloh/rabbitmq/exchange/direct/consumer/DirectLog.java
@@ -0,0 +1,40 @@
+package org.shiloh.rabbitmq.exchange.direct.consumer;
+
+import com.rabbitmq.client.Channel;
+import org.shiloh.rabbitmq.util.RabbitMqUtils;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+import static com.rabbitmq.client.BuiltinExchangeType.DIRECT;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.shiloh.rabbitmq.constant.RabbitMqConstants.DIRECT_EXCHANGE_NAME;
+
+/**
+ * 直接交换机案例 - 日志消息生产者，负责发布日志消息到交换机中
+ *
+ * @author shiloh
+ * @date 2022/4/27 22:14
+ */
+public class DirectLog {
+    public static void main(String[] args) {
+        try {
+            // get channel
+            final Channel channel = RabbitMqUtils.getChannel();
+            // 创建交换机并指定名称和类型
+            channel.exchangeDeclare(DIRECT_EXCHANGE_NAME, DIRECT);
+            // 从控制台读取输入以获取消息数据，并发布到交换机中
+            final Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                final String msg = scanner.next();
+                // 根据不同的 routing key 将消息发送到不同的队列中
+                // 目前有的 routing key 有：info、warning、error
+                channel.basicPublish(DIRECT_EXCHANGE_NAME, "error", null, msg.getBytes(UTF_8));
+                System.out.println("已将消息：" + msg + " 成功发布到交换机：" + DIRECT_EXCHANGE_NAME);
+            }
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ hello-world/src/main/java/org/shiloh/rabbitmq/exchange/direct/consumer/ReceiveLogsDirect2.java
@@ -0,0 +1,53 @@
+package org.shiloh.rabbitmq.exchange.direct.consumer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import org.shiloh.rabbitmq.util.RabbitMqUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import static com.rabbitmq.client.BuiltinExchangeType.DIRECT;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.shiloh.rabbitmq.constant.RabbitMqConstants.DIRECT_EXCHANGE_NAME;
+
+/**
+ * 直接交换机 - 消费者02
+ *
+ * @author shiloh
+ * @date 2022/5/6 20:49
+ */
+public class ReceiveLogsDirect2 {
+    /**
+     * 队列名称
+     */
+    private static final String QUEUE_NAME = "disk";
+
+    /**
+     * routing key
+     */
+    private static final String ROUTING_KEY = "error";
+
+
+    public static void main(String[] args) {
+        // get channel
+        try {
+            final Channel channel = RabbitMqUtils.getChannel();
+            // 声明一个直接交换机
+            channel.exchangeDeclare(DIRECT_EXCHANGE_NAME, DIRECT);
+            // 声明一个队列
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            // 使用 routing key 将队列绑定到交换机
+            channel.queueBind(QUEUE_NAME, DIRECT_EXCHANGE_NAME, ROUTING_KEY);
+
+            // 开始接收消息并消费
+            final DeliverCallback deliverCallback = (tag, msg) -> {
+                final String className = ReceiveLogsDirect2.class.getSimpleName();
+                System.out.println(className + " 接收到的消息为：" + new String(msg.getBody(), UTF_8));
+            };
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, tag -> {});
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
