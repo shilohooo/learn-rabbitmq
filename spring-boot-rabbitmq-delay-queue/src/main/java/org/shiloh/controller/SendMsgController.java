eaea98767b00050d75acc4e79b36031d8bf41a8e
shiloh <lixiaolei595@gmail.com>
2022-05-19 21:47:20 +0800

    feat(延迟队列): 新增基于延迟交换机插件的延迟消息发送接口

--- spring-boot-rabbitmq-delay-queue/src/main/java/org/shiloh/controller/SendMsgController.java
+++ spring-boot-rabbitmq-delay-queue/src/main/java/org/shiloh/controller/SendMsgController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+import static org.shiloh.config.DelayedQueueConfig.PLUGIN_DELAYED_EXCHANGE_NAME;
+import static org.shiloh.config.DelayedQueueConfig.PLUGIN_DELAYED_RK;
+
 /**
  * 生产者 - 延迟消息发送接口
  *
@@ -40,7 +43,13 @@ public class SendMsgController {
     }
 
     /**
-     * 发送指定 TTL 的延迟消息
+     * 发送指定 TTL 的延迟消息，使用死信队列作为延迟队列的缺陷：
+     * 若队列中存在多个设置了 TTL 的队列，而第一个入队的消息 TTL 较长，且随后入队的消息 TTL 较短，
+     * 此时 RabbitMq 只会检查队列中第一个消息是否过期，如果过期则将其转发到死信队列，但由于第一个消息延时很长，
+     * 而其余消息延迟较短，其余消息并不会有限得到执行。
+     * <p>
+     * 这是因为队列是先进先出的数据结构，当第一条消息延迟过长时，剩余的消息便被阻塞在队列中了，
+     * 需要等到第一个消息过期被转发到死信队列后，其余消息才有机会执行。
      *
      * @param msg     消息内容
      * @param ttlTime 消息存活时间，单位：毫秒
@@ -59,6 +68,29 @@ public class SendMsgController {
                     // 设置消息存活时间
                     message.getMessageProperties()
                             .setExpiration(ttlTime);
+                    return message;
+                }
+        );
+    }
+
+    /**
+     * 延迟队列 - 基于延迟交换机插件发送延迟消息
+     *
+     * @param msg     消息内容
+     * @param ttlTime 延迟时间，单位：毫秒
+     * @author shiloh
+     * @date 2022/5/19 21:36
+     */
+    @GetMapping("/delayed-msg-with-plugin")
+    public void sendDelayedMsgWithPlugin(String msg, Integer ttlTime) {
+        log.info("当前时间：{}，发送一条 TTL 为：{} 毫秒的消息给延迟队列：delayed_queue，消息内容：{}", new Date(), ttlTime, msg);
+        this.rabbitTemplate.convertAndSend(
+                PLUGIN_DELAYED_EXCHANGE_NAME,
+                PLUGIN_DELAYED_RK,
+                msg,
+                message -> {
+                    // 发送消息时，设置延迟时间，单位：毫秒
+                    message.getMessageProperties().setDelay(ttlTime);
                     return message;
                 }
         );
